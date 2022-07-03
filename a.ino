a34733356c4a5be14bfc16c165715b52ae2530ab
Abdul Quadir <60433139+Abdul5@users.noreply.github.com>
2022-07-04 02:16:40 +0530

    Add files via upload

---
+++ a.ino
@@ -0,0 +1,44 @@
+#include <Servo.h>
+int trig=10;
+int echo=11;
+Servo s;
+Servo s1;
+int w=A0;
+void setup()
+{
+  pinMode(w,INPUT);
+   pinMode(echo,INPUT);
+  pinMode(trig,OUTPUT);
+  s.attach(8);
+  s1.attach(5);
+  Serial.begin(9600);
+}
+void loop() {
+  int rain=analogRead(w);
+ int duration, distance;
+  digitalWrite (trig, HIGH);
+  delayMicroseconds (10);
+  digitalWrite (trig, LOW);
+  duration = pulseIn (echo, HIGH);
+  distance = (duration/2) / 29.1;
+
+      Serial.print(distance);   
+      Serial.print("cm1");
+      Serial.println();
+if(distance<3){
+  s.write(180);
+    delay(1000); }
+    if(distance<5){
+    s.write(0);
+    delay(2000); 
+    }
+    if(rain<800){
+  s1.write(45);
+    delay(1000); }
+   else{
+    s1.write(0);
+    delay(30); 
+    }
+Serial.println(distance);
+Serial.println(rain);
+}
---
+++ web3.ino
@@ -0,0 +1,72 @@
+#include "ThingSpeak.h"
+#include <ESP8266WiFi.h>
+ 
+const char* ssid= "dlink";
+const char* pass= "12345678900";
+const int trigger = 2;
+const int echo = 0;
+const int trigger1 = 4; //D4
+const int echo1 = 5; //D5
+long T;
+long T1;
+float distanceCM;
+float distanceCM1;
+WiFiClient  client;
+
+unsigned long myChannelField = 998606; // Channel ID
+unsigned long myChannelField1 = 998611;
+const int ChannelField = 1; // Which channel to write data
+const int ChannelField1 = 1; 
+const char * myWriteAPIKey = "L6IDPH5P6VJQ2NUB"; // Your write API Key
+const char * myWriteAPIKey1 = "Z9T5ZUUT1NVP0O3W";
+
+
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(trigger, OUTPUT);
+  pinMode(echo, INPUT);
+  pinMode(trigger1, OUTPUT);
+  pinMode(echo1, INPUT);
+  WiFi.mode(WIFI_STA);
+  ThingSpeak.begin(client);
+}
+void loop()
+{
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+      WiFi.begin(ssid, pass);
+      Serial.print(".");
+      delay(5000);
+    }
+    Serial.println("\nConnected.");
+     Serial.println(WiFi.localIP());
+  }
+  digitalWrite(trigger, LOW);
+  delay(1);
+  digitalWrite(trigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigger, LOW);
+  T = pulseIn(echo, HIGH);
+  distanceCM = T * 0.034;
+  distanceCM = distanceCM / 2;
+  Serial.print("Distance in cm: ");
+  Serial.println(distanceCM);
+  digitalWrite(trigger1, LOW);
+  delay(1);
+  digitalWrite(trigger1, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigger1, LOW);
+  T1 = pulseIn(echo1, HIGH);
+  distanceCM1 = T1 * 0.034;
+  distanceCM1 = distanceCM1 / 2;
+  Serial.print("Distance in cm1: ");
+  Serial.println(distanceCM1);
+  ThingSpeak.writeField(myChannelField, ChannelField, distanceCM, myWriteAPIKey);
+  ThingSpeak.writeField(myChannelField1, ChannelField1, distanceCM1, myWriteAPIKey1);
+  delay(1000);
+}
